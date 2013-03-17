539960a89660ab4160afd19c38ef1c5082812436
Trevor Marsh <trevormarsh@email.com>
2013-03-16 20:18:14 -0600

    Creating the database and database creation script

---
+++ db_create.sql
@@ -0,0 +1,12 @@
+﻿-- Trevor Marsh
+-- 3/16/13
+-- Database creation setup
+
+USE master
+
+IF EXISTS (SELECT * FROM sys.databases d WHERE d.name = 'CLEANCODE')
+DROP DATABASE CLEANCODE
+
+GO
+
+USE master
