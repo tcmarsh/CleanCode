aea93e8563c07918b02cf0c4e33fc0a64a34f21d
Trevor Marsh <trevormarsh@email.com>
2013-04-21 13:33:05 -0600

    Create login test and method function

---
+++ CleanCodeWiki/DataAccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+
+namespace CleanCodeWiki
+{
+    public class DataAccess
+    {
+        public static user Login(string email, string password)
+        {
+            CleanCodeEntities context = new CleanCodeEntities();
+            user requestedUser = null;
+
+            var query = from u in context.users
+                        where u.email == email
+                        select u;
+
+            IEnumerable<user> users = query.ToList();
+            Rfc2898DeriveBytes derive = null;
+            foreach (user testUser in users)
+            {
+                derive = new Rfc2898DeriveBytes(password, testUser.salt);
+                if (!derive.GetBytes(8).Equals(testUser.password))
+                {
+                    requestedUser = testUser;
+                    break;
+                }
+            }
+
+            if (requestedUser == null)
+            {
+                throw new InvalidOperationException("Invalid email and password combination");
+            }
+
+            return requestedUser;
+        }
+    }
+}
---
+++ CleanCodeWikiTest/DataAccessTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CleanCodeWiki;
+using System.Security.Cryptography;
+
+namespace CleanCodeWikiTest
+{
+    [TestClass]
+    public class DataAccessTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            context = new CleanCodeEntities();
+            email = "test@test.com";
+            password = "testpassword";
+
+            derive = new Rfc2898DeriveBytes(password, 8);
+            salt = derive.Salt;
+
+            newUser = new user();
+            newUser.email = email;
+            newUser.last_name = "test";
+            newUser.salt = salt;
+            newUser.password = derive.GetBytes(8);
+            newUser.role_id = 3;
+
+            newUser = context.users.Add(newUser);
+            context.SaveChanges();
+        }
+       
+        private string email;
+        private string password;
+        private byte[] salt;
+        private Rfc2898DeriveBytes derive;
+        private user newUser;
+        private CleanCodeEntities context;
+
+        [TestMethod]
+        public void LoginTest()
+        {
+
+            user loginUser = DataAccess.Login(email, password);
+
+            context.users.Remove(newUser);
+            context.SaveChanges();
+
+            Assert.IsNotNull(loginUser);
+
+            Assert.AreEqual(newUser.email, loginUser.email);
+
+            try
+            {
+                loginUser = DataAccess.Login(email, "not" + password);
+            }
+            catch (InvalidOperationException exception)
+            {
+                Console.WriteLine("Catching the expected invalid operation exception");
+                Console.WriteLine(exception);
+                loginUser = null;
+            }
+
+            Assert.IsNull(loginUser);
+        }
+    }
+}
